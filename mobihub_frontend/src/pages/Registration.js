f95363357f24c795227d55b61ce10c85535048ca
Yash <yashnigam1801@gmail.com>
2024-03-03 16:18:40 +0530

    complete

--- mobihub_frontend/src/pages/Registration.js
+++ mobihub_frontend/src/pages/Registration.js
@@ -1,60 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import "../css/Registration.css";
+
 const Registration = () => {
-  return (
-    <>
-      <div id="Secound_box2">
-        <div id="Secound_first_box2">
-          <div id="input_box_div2" >
-            <div id="logo_img2">
-              <img src="./images/main_logo.png" alt="" />
-              {/* <h1>MobiHub</h1> */}
+    return (
+        <div className="container">
+            <div className="image">
+                <img src="https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="abc" />
             </div>
-            <h1 id="Wel_come2">Welcome Back</h1>
-            <div id="Regi_with_Details2">
-              <p>__</p>
-              <h5>Registration with Your Detials</h5>
-              <p>__</p>
+            <div className="form">
+                <h2>Registration Form</h2>
+                <div className="input-group">
+                    <input type="text" id="name" placeholder="Name" required />
+                    <input type="text" id="lastname" placeholder="Last Name" required />
+                </div>
+                <div className="input-group">
+                    <input type="email" id="email" placeholder="Email" required />
+                    <input type="password" id="password" placeholder="Password" required />
+                </div>
+                <div className="input-group">
+                <input type="text" id="address" placeholder="Address" required />
+                </div>
+                
+                <div className="checkbox">
+                    <input type="checkbox" id="keepLoggedIn" />
+                    <label htmlFor="keepLoggedIn">Keep me logged in</label>
+                </div>
+                <button className='btnn' type="button">Register Now</button>
+                <div className="already-customer">
+                    <p>Already a customer? <Link to="/loginpage">Please Login in</Link> <FaArrowRight /></p>
+                </div>
             </div>
-            <div id="Name_input2">
-              <span>Name</span><br />
-              <input type="text" placeholder="Enter Your Name" />
-            </div>
-            <div id="Last_Name2">
-              <span>Last Name</span><br />
-              <input type="text" placeholder="Enter Your Last  Name" />
-            </div>
-            <div id="Last_Name2">
-              <span>Email</span><br />
-              <input type="text" placeholder="Enter Your Email" />
-            </div>
-            <div id="Last_Name2">
-              <span>Password</span><br />
-              <input type="text" placeholder="Enter Your Password" />
-            </div>
-            <div id="Last_Name2">
-              <span>Address</span><br />
-              <input type="text" placeholder="Enter Your Address" />
-            </div>
-            <div id="keep_box2">
-              <input type="checkbox" /> 
-              &nbsp;&nbsp;
-              <p id="Keep_line_box2">Keep me logged in </p>
-            </div>
-            <button id="Registration_btn2">
-              <h1>Registration Now</h1>
-            </button>
-          </div>
         </div>
-        <div id="Secound_sec_box2">
-
-          <img src="./images/mobile_img.jpg" alt=" " />
-
-
-          <img src="./images/login_image.jpg" alt=" " />
-
-        </div>
-      </div>
-    </>
-  );
+    );
 }
 
 export default Registration;
